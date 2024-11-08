bf0bc78143ae8bd6baa0beabff8344a85fbb1aae
silviac <silviacgil@gmail.com>
2024-11-08 17:48:15 +0100

    Update course-script.js

--- scripts/course-script.js
+++ scripts/course-script.js
@@ -1,1 +1,13 @@
+// scripts/course-script.js
+
+document.addEventListener("DOMContentLoaded", function () {
+    document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+        anchor.addEventListener("click", function (e) {
+            e.preventDefault();
+            document.querySelector(this.getAttribute("href")).scrollIntoView({
+                behavior: "smooth"
+            });
+        });
+    });
+});
 
